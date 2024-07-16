72dd3891cbc792fdfdd6fe44bbee35629e4cbb1b
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-07-16 18:22:11 -0400

    Add keel

---
+++ StarRailBattleSim/src/relicSetBonus/BrokenKeel.java
@@ -0,0 +1,22 @@
+package relicSetBonus;
+
+import battleLogic.Battle;
+import characters.AbstractCharacter;
+import powers.PermPower;
+
+public class BrokenKeel extends AbstractRelicSetBonus {
+    public BrokenKeel(AbstractCharacter owner) {
+        super(owner);
+    }
+
+    public void onCombatStart() {
+        PermPower power = new PermPower();
+        power.bonusCritDamage = 10;
+        power.name = "Broken Keel Crit Dmg Bonus";
+        power.maxStacks = 999;
+        for (AbstractCharacter character : Battle.battle.playerTeam) {
+            character.addPower(power);
+        }
+    }
+
+}
