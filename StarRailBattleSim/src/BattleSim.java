29409ec1f9cbe04118fe13d81cf148a8d734e8d1
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-07-20 21:10:24 -0400

    update testing code

--- StarRailBattleSim/src/BattleSim.java
+++ StarRailBattleSim/src/BattleSim.java
@@ -21,17 +21,17 @@ public class BattleSim {
         Battle battle = new Battle();
         Battle.battle = battle;
 
-        //battle.setPlayerTeam(TingyunYunliRobinHuohuo());
-        //battle.setPlayerTeam(TopazYunliRobinHuohuo());
-        //battle.setPlayerTeam(MarchYunliRobinHuohuo());
-        //battle.setPlayerTeam(SparkleYunliRobinHuohuo());
-        //battle.setPlayerTeam(SparkleYunliTingyunHuohuo());
-        battle.setPlayerTeam(new PelaYunliTingyunHuohuoTeam().getTeam());
+        battle.setPlayerTeam(new TingyunYunliRobinHuohuoTeam().getTeam());
+        //battle.setPlayerTeam(new TopazYunliRobinHuohuoTeam().getTeam());
+        //battle.setPlayerTeam(new MarchYunliRobinHuohuoTeam().getTeam());
+        //battle.setPlayerTeam(new SparkleYunliRobinHuohuoTeam().getTeam());
+        //battle.setPlayerTeam(new SparkleYunliTingyunHuohuoTeam().getTeam());
+        //battle.setPlayerTeam(new PelaYunliTingyunHuohuoTeam().getTeam());
 
         ArrayList<AbstractEnemy> enemyTeam = new ArrayList<>();
         enemyTeam.add(new PhysWeakEnemy(0, 2));
         enemyTeam.add(new PhysWeakEnemy(1, 2));
-        enemyTeam.add(new PhysWeakEnemy(2, 2));
+        //enemyTeam.add(new PhysWeakEnemy(2, 2));
         battle.setEnemyTeam(enemyTeam);
 
         battle.Start(5050);
