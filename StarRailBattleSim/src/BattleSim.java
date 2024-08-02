47b371d6b5903148b0263b06230fbca58a26fb28
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-08-02 18:00:47 -0400

    update message

--- StarRailBattleSim/src/BattleSim.java
+++ StarRailBattleSim/src/BattleSim.java
@@ -92,7 +92,7 @@ public class BattleSim {
         enemyTeams.add(new WindWeakTarget1());
         enemyTeams.add(new FireWindImgWeakTarget1());
 
-        String notes = "Notes: E0S0 Feixiao, E0S0 other 5 stars. E6 4 stars. Maxed out traces and levels. Enemies are level 95. Relics are +15 with relatively relatable rolls. Simulations run for 50 cycles to reduce the impact of RNG and leftover AV/Energy at the end of combat.";
+        String notes = "Notes: E0S0 Feixiao, E0S0 other 5 stars. E6 4 stars. Maxed out traces and levels. Enemies are level 95. Relics are +15 with relatively relatable rolls. Simulations run for 50 cycles to reduce the impact of RNG and leftover AV/Energy at the end of combat. \n Note on the rotation: in this simulation, Feixiao will wait until she can break the enemy/the enemy is broken and the team's buffs/debuffs are present before using Ultimate. This results in wasting some stacks, but waiting to maximize each ultimate's damage is more damage in the long term.";
         Report report = new Report(baselineTeam, otherTeams, enemyTeams, 5050, notes);
         report.generateCSV();
     }
